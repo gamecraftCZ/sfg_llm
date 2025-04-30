9843d1b58b23e90d13aee0e20b7ca84da06930d8
Patrik Vacal <patrik.vacal@gmail.com>
2025-04-30 17:16:17 +0200

    Implement LLM try again on ratelimit exception

--- sfg_audiobook/components/abstract/AbstractStructuredLLMComponent.py
+++ sfg_audiobook/components/abstract/AbstractStructuredLLMComponent.py
@@ -2,6 +2,7 @@ import json
 from abc import ABC
 from time import sleep
 
+import openai
 from jinja2 import Template
 import litellm
 import os
@@ -36,15 +37,18 @@ class AbstractStructuredLLMComponent(AbstractComponent, ABC):
         self._model_supported_params = litellm.get_supported_openai_params(model=self._model)
 
         # Set API key
-        api_provider = self._model.split("/")[0]
-        env_api_key_name = f"{api_provider.upper()}_API_KEY"
+        self._api_provider = self._model.split("/")[0].lower()
+        env_api_key_name = f"{self._api_provider.upper()}_API_KEY"
         self._api_key = params.get("api_key") or os.environ.get(env_api_key_name)
         if params.get("api_key"):
             os.environ[env_api_key_name] = self._api_key
 
         if not self._api_key:
-            raise ValueError(
-                f"API key for {api_provider} is required. Set it in the params or as an environment variable {env_api_key_name}.")
+            if self._api_provider.lower() == "ollama":  # Ollama does not require api key
+                pass
+            else:
+                raise ValueError(
+                    f"API key for {self._api_provider} is required. Set it in the params or as an environment variable {env_api_key_name}.")
 
     @staticmethod
     def get_attributes_help_text() -> str:
@@ -57,7 +61,7 @@ class AbstractStructuredLLMComponent(AbstractComponent, ABC):
     def setup(self, data: PipelineData):
         pass
 
-    def predict(self, data: PipelineData, text: str, TargetModel: type(BaseModel)):
+    def predict(self, data: PipelineData, text: str, TargetModel: type(BaseModel), context_window_size: int = None):
         # Render the template with the text
         system_prompt = self._system_prompt_template.render({"data": data, "text": text})
         content_prompt = self._content_prompt_template.render({"data": data, "text": text})
@@ -66,21 +70,36 @@ class AbstractStructuredLLMComponent(AbstractComponent, ABC):
         # Try 5 times in case something bad happens
         for i in range(5):
             try:
-                res = litellm.completion(
-                    model=self._model,
-                    messages=[
-                        {"role": "system", "content": system_prompt},
-                        {"role": "user", "content": content_prompt}
-                    ],
-                    response_format=TargetModel,  # litellm sucks with gemini and this does not make output json necessary valid
-                    # response_format={"type": "json_object"},
-                    seed=42,  # Try to make the model as deterministic as possible between calls (even tho it is not possible: https://platform.openai.com/docs/advanced-usage/reproducible-outputs)
-                )
-            except Exception as e:
-                print(f"Error calling LLM API: {e}")
+                if self._api_provider == "ollama":
+                    res = litellm.completion(
+                        model=self._model,
+                        messages=[
+                            {"role": "system", "content": system_prompt},
+                            {"role": "user", "content": content_prompt}
+                        ],
+                        format=TargetModel.model_json_schema(),
+                        num_ctx=context_window_size if context_window_size else 8192,  # Set larger context window for the Ollama server to use with the model
+                    )
+                else:
+                    res = litellm.completion(
+                        model=self._model,
+                        messages=[
+                            {"role": "system", "content": system_prompt},
+                            {"role": "user", "content": content_prompt}
+                        ],
+                        response_format=TargetModel,  # litellm sucks with gemini and this does not make output json necessary valid
+                        # response_format={"type": "json_object"},
+                        seed=42,  # Try to make the model as deterministic as possible between calls (even tho it is not possible: https://platform.openai.com/docs/advanced-usage/reproducible-outputs)
+                        allowed_openai_params=['seed'],
+                    )
+            except openai.RateLimitError as e:
+                print(f"LLM API rate limit hit: {e}")
                 if i == 4:
                     raise e
-                sleep(60)  # Sleep one second
+                sleep(60)  # Sleep one minute
+            except litellm.InternalServerError as e:
+                print(f"LLM API internal server error: {e}")
+                sleep(60)  # Sleep one minute and try again
 
         # Process the response
         try:
